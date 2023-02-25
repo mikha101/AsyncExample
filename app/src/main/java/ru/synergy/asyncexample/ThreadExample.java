ba32ea856899fbe6727bf6a0f3a0870b1ef280b9
«Elena <helenvit599@gmail.com>
2023-02-25 23:00:02 +0500

    Finished sparrow counter example

--- app/src/main/java/ru/synergy/asyncexample/ThreadExample.java
+++ app/src/main/java/ru/synergy/asyncexample/ThreadExample.java
@@ -1,14 +1,61 @@
 package ru.synergy.asyncexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class ThreadExample extends AppCompatActivity {
 
+    int mCounter;
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            TextView mInfoTextView = (TextView) findViewById(R.id.textViewInfo);
+            mInfoTextView.setText("Сегодня варон было" + mCounter + " штук");
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread_example);
+
+
+
+    }
+
+    public void  onClick(View v){
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                long endTime = System.currentTimeMillis() + 20 * 1000;
+                while (System.currentTimeMillis() < endTime){
+                    synchronized (this){
+                        try{
+                            wait(endTime-System.currentTimeMillis());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Log.i("SPARROWS", ("Сегодня варон было" + mCounter++ + " штук"));
+
+                    handler.sendEmptyMessage(0);
+                }
+
+                    // Так писать нельзя
+//                TextView mInfoTextView = (TextView) findViewById(R.id.textViewInfo);
+//                mInfoTextView.setText("Сегодня варон было" + mCounter++ + " штук");
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }
